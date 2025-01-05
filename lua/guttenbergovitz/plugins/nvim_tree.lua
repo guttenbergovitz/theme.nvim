daba52de82c495a0311e3d7d77deb41a0f8fbb5a
(ツ) <guttenbergovitz@gmail.com>
2025-01-05 22:46:38 +0100

    style: Update colors for NvimTree plugin

--- lua/guttenbergovitz/plugins/nvim_tree.lua
+++ lua/guttenbergovitz/plugins/nvim_tree.lua
@@ -11,24 +11,23 @@ function M.setup()
     -- Basic elements
     vim.api.nvim_set_hl(0, "NvimTreeNormal", { fg = colors.fg, bg = colors.bg_dark })
     vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { fg = colors.bg_dark, bg = colors.bg_dark })
-    vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { fg = colors.border, bg = colors.bg_dark })
+    vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { fg = colors.bg_dark })
     
     -- Files and folders
     vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = colors.fg })
-    vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = colors.fg, bold = true })
+    vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = colors.fg })
     vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { fg = colors.fg_dark })
     vim.api.nvim_set_hl(0, "NvimTreeFileIcon", { fg = colors.fg_dark })
-    vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = colors.fg })
     
     -- Special files
-    vim.api.nvim_set_hl(0, "NvimTreeImageFile", { fg = colors.orange })
-    vim.api.nvim_set_hl(0, "NvimTreeSpecialFile", { fg = colors.orange, underline = true })
-    vim.api.nvim_set_hl(0, "NvimTreeSymlink", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "NvimTreeSpecialFile", { fg = colors.type })
+    vim.api.nvim_set_hl(0, "NvimTreeSymlink", { fg = colors.type })
     
     -- Git integration
-    vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = colors.green })
-    vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = colors.yellow })
-    vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = colors.red })
+    vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = colors.string })
+    vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = colors.func })
+    vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = colors.keyword })
     vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", { fg = colors.fg_dark })
 end
 
